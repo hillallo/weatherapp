f27613a2a3f3c1f8dab10efa452fa642401b60bf
Ilyas Göcmenoglu <figo@kth.se>
2020-11-25 20:16:08 +0100

    h

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
 import React, { useEffect, useState } from "react";
-import keys from "./keys";
-import { FlatList, StyleSheet, Text, View, Dimensions, Image, TextInput, Button } from 'react-native';
+import keys from "./keyandimages";
+import { FlatList, StyleSheet, Text, View, Dimensions, Image, TextInput, Button, SafeAreaView, KeyboardAvoidingViewComponent, AsyncStorage} from 'react-native';
 import { Input, Divider } from 'react-native-elements';
 import * as firebase from 'firebase';
 import * as FileSystem from 'expo-file-system';
-
+import keyandimages from "./keyandimages";
+import NetInfo from "@react-native-community/netinfo";
 
 const api = {
-  base: keys.BASE_URL,
+  base: keyandimages.BASE_URL,
 };
 
+//Database
+var firebaseConfig = {
+  apiKey: "AIzaSyDbhVNxocJUldPoHxyCvkHKozxn-X2C5TA",
+  authDomain: "weatherapp-f56f9.firebaseapp.com",
+  databaseURL: "https://weatherapp-f56f9.firebaseio.com",
+  projectId: "weatherapp-f56f9",
+  storageBucket: "weatherapp-f56f9.appspot.com",
+  messagingSenderId: "50169328296",
+  appId: "1:50169328296:web:75ba237235b71395d5489d",
+  measurementId: "G-ME09PP722W"
+};
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+///////////////
 function App() {
-
-  const dateBuild = (d) => {
-    let date = String(new window.Date());
-    date = date.slice(3, 15);
-    return date;
-  };
 
   const [query, setQuery] = useState("");
   const [query1, setQuery1] = useState("");
@@ -24,10 +34,53 @@ function App() {
   const [time, setTime] = useState("");
 
   var weathers = [];
-  var test = [];
   var placehold;
-  var curImage = "";
+  var test;
 
+
+  const load = async () => {
+    try {
+      let name = JSON.parse(await AsyncStorage.getItem("myObj"));
+      let time = JSON.parse(await AsyncStorage.getItem("myTime"))
+      if(name.length > 5){
+        setWeather(name)
+        setTime(time)
+      }
+    } catch (err) {
+      alert(err)
+    }
+  }
+
+  const save = async () => {
+    try {
+      let weather_serialized = JSON.stringify(weather)
+      let time_serialized = JSON.stringify(time);
+
+      await AsyncStorage.setItem("myObj", weather_serialized)
+      await AsyncStorage.setItem("myTime", time_serialized)
+
+      console.log("Save", await AsyncStorage.getItem("myTime"))
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    console.log(NetInfo.fetch().then((state) =>{
+      console.log(state.isConnected)
+      test = state.isConnected
+      console.log(test)
+    }));
+    if (test === true) {
+      firebase.database().ref("weather/weather").on('value', (snapshot) => {
+        weathers = snapshot.val();
+        setWeather(snapshot.val().timeSeries)
+        setTime(snapshot.val().approvedTime);
+      });
+    } else {
+      load()
+    }
+  }, []);
 
   const checkFloat = () => {
     var str1 = query
@@ -40,23 +93,30 @@ function App() {
   }
 
   const search = () => {
-    console.log("check float")
     if (checkFloat() != -1) {
       fetch(`${api.base}api/category/pmp3g/version/2/geotype/point/lon/${query}/lat/${query1}/data.json`)
         .then((res) => res.json())
         .then((result) => {
           setTime(result.approvedTime);
           setWeather(result.timeSeries);
-        }).catch(error => {
+          firebase
+            .database()
+            .ref('weather/weather')
+            .set({
+              weather: result
+            });
+        }).then(() => {
+          save()
+        })
+        .catch(error => {
           console.log(error)
-          alert('Error')
+          alert(error)
         });
     }
-
   };
 
   return (
-    <View>
+    <SafeAreaView>
       {weather.map(name => {
         name.parameters.map(name1 => {
           if (name1.name == "t") {
@@ -67,25 +127,27 @@ function App() {
       <View>
         <TextInput
           type="text"
-          placeholder="Enter text here..."
+          placeholder="Longitude"
           className="search-bar"
           onChangeText={(e) => setQuery(e)}
           value={query}
           returnKeyType="done"
           style={{
-            marginTop: 40,
+            marginTop: 10,
+            borderWidth: 1,
+            fontSize: 20
           }}
 
         />
         <TextInput
           type="text"
-          placeholder="longitude"
+          placeholder="Latitude"
           onChangeText={(e) => setQuery1(e)}
           value={query1}
           returnKeyType="done"
           style={{
-            marginTop: 40,
-            marginBottom: 20
+            borderWidth: 1,
+            fontSize: 20
           }}
         >
         </TextInput>
@@ -94,14 +156,13 @@ function App() {
           title="Submit"
           color="#841584"
         />
-
       </View>
 
       <Text
         style={{
           fontWeight: 'bold'
         }}>
-        Första: {query}
+        Latitude: {query}
       </Text>
       <Text
         style={{
@@ -109,8 +170,12 @@ function App() {
         }}>
         Longitude: {query1}
       </Text>
-      <Text>
-        approvedTime: {time}
+      <Text
+        style={{
+          fontWeight: 'bold',
+          fontSize: 20
+        }}>
+        Time: {time}
       </Text>
 
       <FlatList
@@ -121,22 +186,21 @@ function App() {
               {{
                 fontWeight: "bold",
                 backgroundColor: 'green',
-                width: Dimensions.get('window').width,
+                width: Dimensions.get('window').width / 1.09,
                 color: 'white',
                 padding: 10,
-                borderRadius: 10
+                borderRadius: 10,
               }}>
               Time: {item.validTime}     Temperature: {item.parameters.map(nameq => {
                 if (nameq.name == "tcc_mean") {
                   placehold = nameq.values[nameq.values.length - 1]
-                  console.log(placehold)
                 }
                 if (nameq.name == "t") {
                   return (
                     <Text>
                       {nameq.values} °C
                       <Image
-                        //source={require(`./assets/${placehold}.png`)}
+                        source={keyandimages.images[placehold]}
                         style={{
                           height: 50,
                           width: 80
@@ -155,7 +219,7 @@ function App() {
         }>
 
       </FlatList>
-    </View>
+    </SafeAreaView>
   );
 }
 
